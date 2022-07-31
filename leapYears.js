65e97bb269152225a2f07fa926d6d916e5859d25
inan yigit <inanygt@hotmail.com>
2022-07-31 13:31:53 +0200

    finished leapyears exc

--- leapYears.js
+++ leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+   if (year%400 == 0) {
+      return true
+   } else if (year%100 == 0) {
+      return false
+   } else if (Number.isInteger(year/4)) {
+      return true;
+   } else {
+      return false;
+   }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+// npm test leapyears.spec
